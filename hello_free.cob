e97c71a34b773161d958736fb16824d9d6a43e7a
piotr.jaskulski@gmail.com <piotr.jaskulski@gmail.com>
2021-04-06 16:35:47 +0200

    test

---
+++ hello_free.cob
@@ -0,0 +1,11 @@
+       >>SOURCE FORMAT FREE 
+IDENTIFICATION DIVISION.
+PROGRAM-ID. hello.
+PROCEDURE DIVISION.
+Begin.
+  DISPLAY "Hello World!".
+  PERFORM DisplayGreeting 10 TIMES.
+  STOP RUN.
+
+DisplayGreeting.
+DISPLAY "Pozdrawia język COBOL".
